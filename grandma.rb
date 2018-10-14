9576c9ea5d9a16df41d673395eba40722cf0bc41
Ji S <j2yshin@gmail.com>
2018-10-14 17:59:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,9 @@
 def speak_to_grandma(string)
   if string != string.upcase
     "HUH?! SPEAK UP, SONNY!"
+    elsif string = string.upcase
+    "NO, NOT SINCE 1938!"
+    elsif string == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   end
 end
